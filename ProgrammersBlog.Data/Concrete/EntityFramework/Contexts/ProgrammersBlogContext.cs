92e622e54f1364041a5cd8ad4e9f61307558c64b
BabekAga <82724876+BabekAga@users.noreply.github.com>
2022-08-15 19:19:51 +0400

    Create data layer

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
+{
+    public class ProgrammersBlogContext:DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Repositories/EfArticleRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Repositories
+{
+    public class ArticleRepository : EfEntityRepositoryBase<Article>, IArticleRepository
+    {
+        public ArticleRepository(DbContext context) : base(context)
+        {
+
+        }
+    }
+}

6dfe1d36788db59e90351be94f602b1f78282cc0
BabekAga <82724876+BabekAga@users.noreply.github.com>
2022-08-25 15:18:53 +0400

    Finished Service Layer

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "b", "a", "b", "b", "c", "c" };
+            Dictionary<string, int> dic = new Dictionary<string,int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                        count++;
+                }
+                if (!dic.ContainsKey(array[i]))
+                    dic.Add(array[i], count);
+            }
+            foreach (var item in dic)
+                Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
---
+++ deneme/deneme/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //var numbers2 = numbers.Select(i => i);
+            var numbers3 = numbers.Where(i => i % 2 == 0).OrderBy(i => i);
+            //var numbers3 = numbers.Where(i => i % 2 == 0).OrderByDescending(i => i); ---> coxdan aza dogru siralayir
+            foreach (int i in numbers3)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            //try
+            //{
+            //    int[] myNumbers = { 1, 2, 3 };
+            //    Console.WriteLine(myNumbers[10]);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("asdcdas");
+            //}
+
+
+
+
+        }
+    }
+}
+/
